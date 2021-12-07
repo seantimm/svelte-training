11ddca035f9e8ca32f004d1a55733616af682cb4
Sean Timm <sean@seantimm.com>
2021-12-07 13:55:08 -0700

    Add a wrap up slide

---
+++ src/slides/11/index.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Slide from "../Slide.svelte";
+</script>
+
+<Slide title="And more...">
+  <ul>
+    <li>
+      Svelte Tutorial: <a href="https://svelte.dev/tutorial/basics"
+        >https://svelte.dev/tutorial/basics</a
+      >
+    </li>
+    <li>
+      Svelte Docs: <a href="https://svelte.dev/docs">https://svelte.dev/docs</a>
+    </li>
+    <li>
+      Presentation Code: <a href="https://github.com/seantimm/svelte-training"
+        >https://github.com/seantimm/svelte-training</a
+      >
+    </li>
+    <li>Related things: SvelteKit, Routify, Vite, PostCSS, Tailwind</li>
+  </ul>
+</Slide>
+
+<style>
+  li {
+    font-size: 2em;
+    margin: 0.5em auto;
+  }
+
+  li a {
+    display: block;
+  }
+</style>
